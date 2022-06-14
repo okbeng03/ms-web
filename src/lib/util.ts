2de6ea030bd4712664b35978940a8a59bf27ab39
chb.wang <chb.wang@hnair.com>
2022-06-14 16:49:29 +0800

    feat: 抽取tags解析代码

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -3,3 +3,20 @@ const regImageFile = /(jpg|jpeg|png|gif|svg|bmp|pic)$/
 export const isImageFile = function(type: string) {
   return regImageFile.test(type)
 }
+
+export const parseTagging = function(tags) {
+  if (tags) {
+    if (Array.isArray(tags[0])) {
+      tags = tags[0]
+    }
+
+    const tagging = {}
+    tags.forEach(item => {
+      tagging[item.Key] = item.Value
+    })
+
+    return tagging
+  }
+  
+  return {}
+}
